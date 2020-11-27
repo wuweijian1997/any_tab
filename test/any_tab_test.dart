58d92c534aeb2b1b57e33ad1e7c9c542a825dc62
luoji <wuweijian1997@gmail.com>
2020-11-27 18:48:03 +0800

    create package

---
+++ test/any_tab_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:any_tab/any_tab.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
